02d7291f6a1194a1e9013ca535b12030c1b1f039
Ihsan Kurniawan <ikurniawanlubis@gmail.com>
2021-06-15 15:24:13 +0700

    add charge method

--- index.js
+++ index.js
@@ -18,17 +18,15 @@ let core = new midtransClient.CoreApi({
     clientKey: REACT_APP_MIDTRANS_CLIENT_KEY
 });
 
-var cardData = {
-    "card_number": '4811111111111114',
-    "card_exp_month": '02',
-    "card_exp_year": '2025',
-    "card_cvv": '123',
-    "client_key": REACT_APP_MIDTRANS_CLIENT_KEY,
-};
+app.get('/get_token', async (req, res) => {
 
-
-
-app.get('/', async (req, res) => {
+    var cardData = {
+        "card_number": '4811111111111114',
+        "card_exp_month": '02',
+        "card_exp_year": '2025',
+        "card_cvv": '123',
+        "client_key": REACT_APP_MIDTRANS_CLIENT_KEY,
+    };
 
     try {
         const x = await core.cardToken(cardData)
@@ -38,6 +36,34 @@ app.get('/', async (req, res) => {
         })
     } catch (error) {
         console.log(error)
+        res.send('nay')
+    }
+})
+
+app.post('/charge_cc/:token_id', async (req, res) => {
+    const { token_id } = req.params
+    const data = {
+        "payment_type": "credit_card",
+        "transaction_details": {
+            "gross_amount": 12145,
+            "order_id": "test-transaction-54321",
+        },
+        "credit_card": {
+            "token_id": token_id, // change with your card token
+            "authentication": true
+        }
+    }
+
+    try {
+        const x = await core.charge(data)
+        console.log(x)
+        res.send({
+            data: x,
+            token_id
+        })
+    } catch (error) {
+        console.log(error)
+        console.log(token_id)
         res.send('nay')
     }
 })
